b5ca761b9a4c2e0f0f28f559eca2b828e6916ef2
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-06-21 15:11:52 -0700

    Continued set up of Image View Controller

--- instagramClone/ImageViewController.swift
+++ instagramClone/ImageViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ImageViewController: UIViewController {
+class ImageViewController: UIViewController, Setup{
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    lazy var imagePicker = UIImagePickerController()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup()
+        setupAppearance()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupAppearance(){
+        self.imageView.layer.cornerRadius = 5.0
+        
     }
-
-
+    func setup(){
+        self.tabBarItem.title = "Instagram Clone"
+        
+    }
+    
+    func presentActionSheet(){
+        let actionSheet = UIAlertController(title: "Find Image", message: "Do you want to use your camera or photo library?", preferredStyle: .Alert)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .Default) { (action) in
+           // self.presentImagePicker(.Camera)
+        }
+        
+        let photosAction = UIAlertAction(title: "Photos", style: .Default) { (action) in
+            //self.presentImagePicker(.PhotoLibrary)
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        
+        
+        actionSheet.addAction(cameraAction)
+        actionSheet.addAction(cancelAction)
+        actionSheet.addAction(photosAction)
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+    }
+ 
 }
 
---
+++ instagramClone/Protocols.swift
@@ -0,0 +1,14 @@
+//
+//  Protocols.swift
+//  instagramClone
+//
+//  Created by Lindsey on 6/21/16.
+//  Copyright © 2016 Lindsey Boggio. All rights reserved.
+//
+
+import Foundation
+
+protocol Setup {
+    func setupAppearance()
+    func setup()
+}
